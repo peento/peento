16791435df07c524bbaa402325fa4072f39a8920
Zongmin Lei <leizongmin@gmail.com>
2014-05-07 17:01:00 +0800

    在debug模式下，支持动态解算资源文件

--- src/middleware/assets.js
+++ src/middleware/assets.js
@@ -5,17 +5,21 @@
  */
 
 module.exports = function (ns) {
-
+  var app = ns('app');
   return function (req, res, next) {
     var asset = ns('asset');
     var n = req._parsedUrl.pathname.slice(8);
     var f = asset[n];
-    if (f) {
-      res.sendfile(f);
-    } else {
-      res.status(404);
-      res.end('File /' + n + ' not found.');
+    if (f) return res.sendfile(f);
+
+    // if is debug mode, try to lookup from asset paths
+    if (app._is_debug) {
+      f = app._fileLookup.asset.resolveSync(n);
+      if (f) return res.sendfile(f);
     }
+
+    res.status(404);
+    res.end('File /' + n + ' not found.');
   }
 
 };
